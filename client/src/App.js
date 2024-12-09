700cc150c476896c8ef0d5df53a92defcd205c25
yordy3001 <yordyovalle3001@gmail.com>
2024-12-09 13:10:28 -0500

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage.js";
+import RegisterPage from './pages/RegisterPage.js';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/Register" element={<RegisterPage />} />
         </Route>
       </Routes>
   
