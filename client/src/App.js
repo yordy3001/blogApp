dfac56c79505dcea9c2a0725ae09e8720839e091
yordy3001 <yordyovalle3001@gmail.com>
2024-12-09 01:20:18 -0500

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+
+
+function App() {
+  return (
+   
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+      </Routes>
+  
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://i0.wp.com/ohmydogblog.com/wp-content/uploads/2015/08/Lucas-and-his-one-block-walk-08.12.15.jpg?resize=1024%2C768&ssl=1"></img>
+        </div>
+          <div className="texts">
+            <h2>When Easy Things Feel Hard</h2>
+            <p className="info">
+              <a className="author">Maggie Marton</a>
+              <time>2021-01-07 17:45</time>
+            </p>
+            <p className="summary">Penny isn’t perfect. She’s super quirky (have you been following along with her Whimzees weirdness on Insta?) and she’s recently discovered how much she enjoys chewing up Barbies and dollhouse accessories.</p>
+          </div>
+        </div>
+    )
+}
